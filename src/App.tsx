f4d1359999ddb8dedd8e4ac62acd017a46d86ab8
paarth <ptaneja@pivotal.io>
2020-08-25 16:20:48 -0400

    Add solutions for workshop 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import Header from "./Header";
 import Game from "./Game";
+import AppContext from './AppContext';
 import AppBanner from "./AppBanner";
 
 const App: React.FC = () => {
-    return <div className="App">
-            <Header/>
-            <AppBanner/>
-            <Game/>
-        </div>;
+    return (
+        <AppContext>
+            <div className="App">
+                <Header/>
+                <AppBanner />
+                <Game/>
+            </div>
+        </AppContext>
+    );
 };
 
 export default App;
